2d69eb98b4182968662ecb11196d10eba444c81c
Sujit Mishra <115906340+snowflakegithubsujit@users.noreply.github.com>
2022-10-17 13:47:33 -0500

    Update streamlit_app.py
    Added item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,10 @@
 import streamlit
-streamlit.title('My Parents New Healthhy Diner')
+streamlit.title('My Mom's New Healthhy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

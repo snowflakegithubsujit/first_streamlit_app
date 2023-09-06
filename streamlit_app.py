2d820bb658eef5045a0aacfec2711cfa85852b71
Sujit Mishra <115906340+snowflakegithubsujit@users.noreply.github.com>
2023-09-06 11:47:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ my_data_row = my_cur.fetchone()
 #streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 
 
